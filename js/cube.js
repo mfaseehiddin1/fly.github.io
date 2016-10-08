d6c6c25c201572c78d82980dc32a5fc20677f0ea
MFaseehuddin <Fasigameplayer@gmail.com>
2016-10-09 00:38:46 +0500

    added a cube navigation

---
+++ js/cube.js
@@ -0,0 +1,20 @@
+function one() {
+    document.getElementById("cube").style.transform = 'rotateY(0deg)'
+}
+function two() {
+    document.getElementById("cube").style.transform = "rotateY(-90deg)";
+}
+
+function three() {
+    document.getElementById("cube").style.transform = "rotateX(90deg)";
+}
+function four() {
+    document.getElementById("cube").style.transform = "rotateX(-90deg)";
+}
+function five() {
+    document.getElementById("cube").style.transform = "rotateY(90deg)";
+}
+
+function six() {
+    document.getElementById("cube").style.transform = "rotateY(180deg)";
+}
