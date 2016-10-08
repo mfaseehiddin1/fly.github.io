1fff19c7379ec97ac012c7a65adb5601fafa20f3
MFaseehuddin <Fasigameplayer@gmail.com>
2016-10-09 01:30:16 +0500

    New interface for project and added projects
    !!!!

---
+++ Projects/IPRT100/js/Js.js
@@ -0,0 +1,8 @@
+var I = Number(document.IPRT.I.value);
+var P = Number(document.IPRT.P.value);
+var R = Number(document.IPRT.R.value);
+var T = Number(document.IPRT.T.value);
+
+function Calc(){
+    document.getElementById("Answer").innerHTML = "asdas";
+}
